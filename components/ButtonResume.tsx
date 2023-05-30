27e8fe940a0796fcc87648594dc440245bfdfabd
novianmw <novian.mulyoadi.w@gmail.com>
2023-05-30 11:21:00 +0700

    update link resume

--- components/ButtonResume.tsx
+++ components/ButtonResume.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 export default function ButtonResume() {
     return (
-        <Link href="https://www.cakeresume.com/pdf/s--xI-doiOL1nF3rfZdqBApIw--/1PyP6.pdf">
+        <Link target="_blank" href="https://www.cakeresume.com/s--DRhkzS6wOaBA0a7VqlkggA--/novian-mw">
             <button className="flex border border-black bg-gradient-to-r from-red-500 to-orange-500 rounded-lg py-2 px-6 font-medium text-lg cursor-pointer ">
                 <Image src={person} width={30} alt="contact" className="mr-2" />
                 Download Resume
