2cd73aa131a76e7fd106d733f6686df3ff643d53
novianmw <novian.mulyoadi.w@gmail.com>
2023-06-14 21:13:29 +0700

    update minor

---
+++ components/SlickPorto.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import { slides } from "../constant"
+import Link from "next/link"
+export default function SlickPorto() {
+    return (
+        <div id="wrapper" className="flex flex-col items-center w-full">
+            {slides.map((slide, index) => (
+                <div key={index} className="w-full flex flex-col items-center p-12">
+                    <Image src={slide.img} alt={slide.id} className="rounded-xl" />
+                    <div className="bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent text-[32px] m-1">{slide.name}</div>
+                    <div className="bg-gradient-to-r from-sky-300 to-white bg-clip-text text-transparent text-[24px] py-5 ">{slide.content}</div>
+                    <Link target="_blank" href={slide.href}>
+                        <button className="border rounded-xl px-4 py-2 hover:text-black hover:bg-white transition-opacity ">{slide.label}</button>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    )
+}
