bd126edff5d8e22347b2f096923315322ca6be48
novianmw <novian.mulyoadi.w@gmail.com>
2023-06-14 21:09:58 +0700

    update penambahan portfolio

--- components/PortfolioHero.tsx
+++ components/PortfolioHero.tsx
@@ -2,12 +2,13 @@ import Image from "next/image"
 import { useState } from "react"
 import { ImgPorto1, ImgPorto2, ImgPorto3 } from "../public/assets/list"
 import Slider from "./Slider"
+import SlickPorto from "./SlickPorto"
 
 export default function PortfolioHero() {
     return (
         <>
-            <section className="flex justify-center items-center">
-                <div className="flex flex-1 text-center flex-col">
+            <section className="flex justify-center items-center pb-32">
+                <div className="flex text-center flex-col">
                     <div className="flex justify-center items-center my-32">
                         <h1 className="text-4xl md:text-6xl md:text-[100px]">
                             My <span className="bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">Portfolio</span> so{" "}
@@ -15,7 +16,8 @@ export default function PortfolioHero() {
                         </h1>
                     </div>
                     <div className="flex justify-center items-center py-14">
-                        <Slider />
+                        {/* <Slider /> */}
+                        <SlickPorto />
                     </div>
                 </div>
             </section>
