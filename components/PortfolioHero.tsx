cd1210238c04becb2a204f6e16acf1db7df9a2c4
novianmw <novian.mulyoadi.w@gmail.com>
2023-08-17 18:14:49 +0700

    update porfolio list

--- components/PortfolioHero.tsx
+++ components/PortfolioHero.tsx
@@ -7,9 +7,9 @@ import SlickPorto from "./SlickPorto"
 export default function PortfolioHero() {
     return (
         <>
-            <section className="flex justify-center items-center pb-32">
+            <section className="flex justify-center items-center m-12">
                 <div className="flex text-center flex-col">
-                    <div className="flex justify-center items-center my-32">
+                    <div className="flex justify-center items-center">
                         <h1 className="text-4xl md:text-6xl md:text-[100px]">
                             My <span className="bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">Portfolio</span> so{" "}
                             <span className="bg-gradient-to-r from-sky-300 to-white bg-clip-text text-transparent">far . . .</span>
--- components/SlickPorto.tsx
+++ components/SlickPorto.tsx
@@ -5,10 +5,10 @@ export default function SlickPorto() {
     return (
         <div id="wrapper" className="flex flex-col items-center w-full">
             {slides.map((slide, index) => (
-                <div key={index} className="w-full flex flex-col items-center p-12">
-                    <Image src={slide.img} alt={slide.id} className="rounded-xl" />
-                    <div className="bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent text-[32px] m-1">{slide.name}</div>
-                    <div className="bg-gradient-to-r from-sky-300 to-white bg-clip-text text-transparent text-[24px] py-5 ">{slide.content}</div>
+                <div key={index} className="w-full flex flex-col items-center p-12 gap-3">
+                    <Image src={slide.img} alt={slide.id} className="rounded-xl w-[800px]" />
+                    <div className="bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent text-[32px]">{slide.name}</div>
+                    <div className="bg-gradient-to-r from-sky-300 to-white bg-clip-text text-transparent text-[24px]">{slide.content}</div>
                     <Link target="_blank" href={slide.href}>
                         <button className="border rounded-xl px-4 py-2 hover:text-black hover:bg-white transition-opacity ">{slide.label}</button>
                     </Link>
